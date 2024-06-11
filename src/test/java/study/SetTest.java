95602603c8d882a7eba7f543a00ad4beee155534
ryeongee <ryeongee24@gmail.com>
2024-06-11 21:57:32 +0900

    Set Collection에 대한 학습 테스트의 DisplayName 변경.

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -1,8 +1,7 @@
 package study;
 
 import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
@@ -14,6 +13,8 @@ import java.util.Set;
  * Created by Robin on 2024/06/11.
  * Description :
  */
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class SetTest {
     private Set<Integer> numbers;
 
@@ -26,7 +27,9 @@ public class SetTest {
         numbers.add(3);
     }
 
+    @Order(1)
     @Test
+    @DisplayName("Set의 크기를 확인.")
     void size_메서드_테스트() {
         //given
         int result = 3;
@@ -36,6 +39,8 @@ public class SetTest {
         Assertions.assertThat(size).isEqualTo(result);
     }
 
+    @Order(2)
+    @DisplayName("Set에 1, 2, 3 값이 존재하는지 확인.")
     @ParameterizedTest
     @ValueSource(ints = {1, 2, 3})
     void contains_메서드_테스트(int number) {
@@ -45,6 +50,8 @@ public class SetTest {
         Assertions.assertThat(isContained).isTrue();
     }
 
+    @Order(3)
+    @DisplayName("Set에 입력 값이 존재하는지 확인. 입력 값에 따라 결과 값이 다른 경우 확인.")
     @ParameterizedTest
     @CsvSource(value = {"1:true", "2:true", "3:true", "4:false", "5:false"}, delimiter = ':')
     void contains_메서드_테스트_2(int input, boolean expected) {

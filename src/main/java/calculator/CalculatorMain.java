4a4f52af258f6707b40132d344e8144794e5ba2c
ryeongee <ryeongee24@gmail.com>
2024-06-12 23:22:13 +0900

    클래스 생성 - 계산기 실행하는 메인 함수.

---
+++ src/main/java/calculator/CalculatorMain.java
@@ -0,0 +1,26 @@
+package calculator;
+
+import java.util.*;
+
+/**
+ * Created by Robin on 2024/06/11.
+ * Description : 계산기 실행하는 메인 함수
+ */
+public class CalculatorMain {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("계산할 문자열을 입력하세요. \nex.2 + 3 * 4 / 2");
+        String text = scanner.nextLine();
+        System.out.printf("입력된 문자열은 %s 입니다.%n", text);
+
+        SequentialInputReader inputSequentialReader = SequentialInputReader.loadReader(text);
+        Queue<Integer> numbers = inputSequentialReader.getNumbers();
+        Queue<Character> operators = inputSequentialReader.getOperators();
+        SequentialCalculator calculator = new SequentialCalculator(numbers, operators);
+        Integer result = calculator.calculate();
+
+        System.out.printf("%s = %s%n", text, result);
+    }
+
+}
